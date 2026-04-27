fb19feaef73384ca9d00c8a9a26c3d2e586c94e7
truongdanghoanganhh-debug-c <truongdanghoanganhh@gmail.com>
2026-04-28 05:11:34 +0700

    Update Chess.js

--- Chess.js
+++ Chess.js
@@ -1,37 +1,11 @@
-var obj = JSON.parse($response.body);
+console.log("Chess script running");
 
-// đảm bảo có data
-if (!obj.data) {
-  obj.data = {};
+if (!$response || !$response.body) {
+  console.log("No response body");
+  $done({});
 }
 
-let time = new Date();
-
-// mapping kiểu “entitlement”
-const premium = {
-  is_premium: 1,
-  type: "diamond",
-  sku: "diamond_yearly_032022",
-  is_apple_subscriber: true,
-  start: Math.floor(time / 1000),
-  expires: Math.floor(new Date(time.setFullYear(time.getFullYear() + 1)) / 1000)
-};
-
-// gán dữ liệu
-obj.data.is_premium = premium.is_premium;
-obj.data.type = premium.type;
-obj.data.sku = premium.sku;
-obj.data.is_apple_subscriber = premium.is_apple_subscriber;
-
-// đảm bảo có date
-if (!obj.data.date) {
-  obj.data.date = {};
-}
-
-obj.data.date.start = premium.start;
-obj.data.date.expires = premium.expires;
-
-// debug
-obj.message = "Handled by script";
+let obj = JSON.parse($response.body);
+obj.test = "ok";
 
 $done({ body: JSON.stringify(obj) });
